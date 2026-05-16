c90e2613f2cca2c19532debf2eee5fe108f0af2b
fachrurrachmanh <hkeemrach@gmail.com>
2026-05-16 10:10:24 +0700

    fix config

--- config.py
+++ config.py
@@ -1,22 +1,16 @@
-# config.py
 
-# =========================
 # APP INFO
-# =========================
-APP_NAME = "FinSight"
-VERSION = "1.0.0"
+NAMA_APP= "FinSight"
+VERSI = "1.0.0"
 
-# =========================
+
 # FILE SETTINGS
-# =========================
 OUTPUT_FOLDER = "outputs/"
 TEMPLATE_FOLDER = "templates/"
 DATABASE_NAME = "finsight.db"
 
-# =========================
 # BENJAMIN GRAHAM RULES
-# =========================
-
+#=======================
 # Minimum Current Ratio
 MIN_CURRENT_RATIO = 2.0
 
@@ -38,41 +32,30 @@ GRAHAM_NUMBER_FACTOR = 22.5
 # Minimum Interest Coverage
 MIN_INTEREST_COVERAGE = 5
 
-# =========================
 # SCORING SYSTEM
 # =========================
-
 SCORE_PASS = 10
 SCORE_WARNING = 5
 SCORE_FAIL = 0
 
-# =========================
+
 # RISK LEVEL
 # =========================
-
 LOW_RISK = "LOW"
 MEDIUM_RISK = "MEDIUM"
 HIGH_RISK = "HIGH"
 
-# =========================
 # REPORT SETTINGS
 # =========================
-
 EXPORT_PDF = True
 EXPORT_EXCEL = True
 EXPORT_JSON = True
 
-# =========================
 # CLI SETTINGS
-# =========================
-
 ENABLE_COLORS = True
 SHOW_LOADING_ANIMATION = True
 
-# =========================
 # GRAHAM CRITERIA WEIGHT
-# =========================
-
 WEIGHT_VALUATION = 30
 WEIGHT_FINANCIAL_HEALTH = 30
 WEIGHT_EARNINGS_STABILITY = 20
